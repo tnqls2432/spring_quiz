1a72e3765aba36858abc655993f10ef95aab96dc
DESKTOP-19UNNDO\82104 <hangun_luk@naver.com>
2022-02-20 16:26:12 +0900

    httpMessageConverter와 어노테이션

--- src/main/java/com/quiz/lesson01/Lesson01Quiz02RestController.java
+++ src/main/java/com/quiz/lesson01/Lesson01Quiz02RestController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,6 +50,7 @@ public class Lesson01Quiz02RestController {
 		board.setTitle("헐 대박");
 		board.setUser("bada");
 		board.setContent("오늘 목요일이었어...");
+		result.add(board);
 		
 		board = new Board();
 		board.setTitle("오늘 데이트 한 이야기 해드릴게요");
@@ -56,6 +59,17 @@ public class Lesson01Quiz02RestController {
 		result.add(board);
 		
 		return result;
+	}
+	
+	// 요청 URL http://localhost:8080/lesson01/quiz02/3
+	@RequestMapping("/3")
+	public ResponseEntity<Board> quiz02_3() {
+		Board board = new Board();
+		board.setTitle("안녕하세요 가입인사 드립니다");
+		board.setUser("hagulu");
+		board.setContent("안녕하세요. 가입했어요. 앞으로 잘 부탁드립니다");
+		
+		return new ResponseEntity<>(board, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	
